9afc7d116a9e93014ab96c6a1316ac79e11a2762
A. Shi <shi.andrewc@gmail.com>
2016-07-12 11:12:14 -0400

    testing scene switch

---
+++ test proj/Assets/Scripts/right_trigger_test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class right_trigger_test : MonoBehaviour {
+
+	SteamVR_TrackedObject controller;
+	// Use this for initialization
+	void Awake () {
+		controller = GetComponent<SteamVR_TrackedObject> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		SteamVR_Controller.Device device = SteamVR_Controller.Input ((int)controller.index);
+
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger partly down");
+		}
+	}
+}
