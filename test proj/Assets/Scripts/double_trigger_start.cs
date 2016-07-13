1e653a46f123cdde5ff82b278bc6d50292be5ffe
A. Shi <shi.andrewc@gmail.com>
2016-07-13 09:27:52 -0400

    floating menu and end, first test

--- test proj/Assets/Scripts/double_trigger_start.cs
+++ test proj/Assets/Scripts/double_trigger_start.cs
@@ -12,12 +12,12 @@ public class double_trigger_start : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         SteamVR_Controller.Device left = SteamVR_Controller.Input(3);
         SteamVR_Controller.Device right = SteamVR_Controller.Input(4);
 
-        if (left.GetPress(SteamVR_Controller.ButtonMask.Trigger) && right.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        if (left.GetPress(SteamVR_Controller.ButtonMask.Trigger) && right.GetPress(SteamVR_Controller.ButtonMask.Trigger))
         {
             Debug.Log("both triggers down");
             SceneManager.LoadScene(1);
