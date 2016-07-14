9d776e7e1ff582e823672421c92eb9bfb80f2cea
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-07-14 12:13:05 -0400

    Throwing Mechanics Basics + Loaded Virus

--- Throwing Mechanics Test/Assets/throw.js
+++ Throwing Mechanics Test/Assets/throw.js
@@ -1,49 +1,52 @@
 ﻿#pragma strict
 
-var mc : GameObject; // The main camera
-var g: boolean = true; // Gravity
+//var mc : GameObject; // The main camera
+var g: boolean = false;
+GetComponent. < Rigidbody > ().useGravity = false; // Gravity
 var canShoot: boolean = false;
 var justShot: boolean = false;
-var cc : CharacterController; // The player
+var cc: CharacterController; // The player
 
-function Start () {}
+function Start() {}
 
- function Update()
- {
- 	var distance: float;
- 	distance = Vector3.Distance(mc.transform.position, GetComponent.<Rigidbody>().transform.position);
+function Update() {
+    while (true) {
+        var distance: float;
+        distance = Vector3.Distance(cc.transform.position, GetComponent. < Rigidbody > ().transform.position);
 
-	if(distance < 8 && !canShoot && !justShot /*|| Input.GetKey("n") && !canShoot && !justShot*/)
-    {
-     	g = false; // turns the gravity on
-        canShoot = true;
-         // If the player is close enough by the sphere and he didn't just shot, the sphere will be picked up.
-         // If the justShot boolean wouldn't be there, you would pick up the sphere immediataly after shooting and you
-         // could not throw it.
+        if (Input.GetKey("p") && !canShoot && !justShot) {
+            canShoot = true;
+            transform.position = cc.transform.position + Vector3.up + Vector3.back;
+            // If the player is close enough by the sphere and he didn't just shot, the sphere will be picked up.
+            // If the justShot boolean wouldn't be there, you would pick up the sphere immediataly after shooting and you
+            // could not throw it.
+        }
+
+        // if (g) {
+        //     GetComponent. < Rigidbody > ().AddForce(Physics.gravity * GetComponent. < Rigidbody > ().mass); // gravity
+        // }
+
+        // if (canShoot &&  !justshot) {
+        //     transform.position = cc.transform.position + cc.transform.forward * 2; // teleports the sphere in front of the player
+        //     GetComponent. < Rigidbody > ().velocity = Vector3.one; // Sets the velocity
+        //     GetComponent. < Rigidbody > ().angularVelocity = Vector3.one*10; // and the rotation of the sphere to zero (rotation is not necessary if you work with a shere).
+        // }
+
+        if (Input.GetKey("b") && canShoot) {
+            print("I made It");
+            g = true;
+            GetComponent. < Rigidbody > ().AddForce(cc.transform.up * 10 + cc.transform.forward * 10, ForceMode.Impulse);
+            GetComponent. < Rigidbody > ().useGravity = true;
+            GetComponent. < Rigidbody > ().angularVelocity = Vector3.one * 5; // shoot
+            canShoot = false;
+            justShot = true;
+        }
+
+        if (transform.position.y < .1 && justShot) {
+            GetComponent. < Rigidbody > ().angularVelocity = Vector3.zero;
+            GetComponent. < Rigidbody > ().velocity = Vector3.zero;
+            GetComponent. < Rigidbody > ().useGravity = false; // reset the justShot boolean, if the sphere is far enough
+        }
+
     }
-
- 	if(g)
- 	{
- 		GetComponent.<Rigidbody>().AddForce(Physics.gravity * GetComponent.<Rigidbody>().mass); // gravity
- 	}
-
-     if(canShoot)
-     {
-     	transform.position = mc.transform.position + mc.transform.forward*2; // teleports the sphere in front of the player
-     	GetComponent.<Rigidbody>().velocity = Vector3.zero; 		   // Sets the velocity
-     	GetComponent.<Rigidbody>().angularVelocity = Vector3.zero; // and the rotation of the sphere to zero (rotation is not necessary if you work with a shere).
-     }
-
-     if(Input.GetKey("b") && canShoot)
-     {
-     	g = true;
-        GetComponent.<Rigidbody>().AddForce(mc.transform.forward * 1500); // shoot
-        canShoot = false;
-        justShot = true;
-     }
-
-     if(justShot && distance > 8)
-     {
-     justShot=false; // reset the justShot boolean, if the sphere is far enough
-     }
- }
+}
