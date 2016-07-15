b49cacbf393f582263a592b9322a9edf628bf5b6
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-07-15 12:50:25 -0400

    VR Throwing Motion Implementation

--- Throwing Mechanics Test/Assets/ThrowingMotionScript.cs
+++ Throwing Mechanics Test/Assets/ThrowingMotionScript.cs
@@ -11,36 +11,61 @@ public class ThrowingMotionScript : MonoBehaviour {
     private SteamVR_TrackedObject trackedObj;
 
     private GameObject pickup;
+    private bool canShoot = false;
+    private bool justShot = false;
     // Use this for initialization
     void Start () {
+
         trackedObj = GetComponent<SteamVR_TrackedObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (controller.GetPressDown(gripButton)&&pickup!=null)
+        if (controller.GetPressDown(gripButton)&&pickup!=null&&!canShoot)
         {
+            print("You just picked it up");
             pickup.transform.parent = this.transform;
             pickup.GetComponent<Rigidbody>().isKinematic = true;
+            canShoot = true;
         }
-        if(controller.GetPressUp(gripButton)&&pickup!=null)
+        if(controller.GetPressUp(gripButton)&&pickup!=null&&canShoot&&!justShot)
         {
+            print("You dropped it");
             pickup.transform.parent = null;
+            pickup.GetComponent<Rigidbody>().useGravity = false;
             pickup.GetComponent<Rigidbody>().isKinematic = false;
+            canShoot = false;
         }
-        if (controller.GetPressDown(triggerButton)&&controller.GetPressDown(gripButton) && pickup != null) {
-            pickup.GetComponent<Rigidbody>().AddForce(Vector3.up * 100+ Vector3.forward*100, ForceMode.Acceleration);
-            pickup.GetComponent<Rigidbody>().angularVelocity = Vector3.one * 100;
+        if (controller.GetPressDown(triggerButton)&&controller.GetPress(gripButton) && pickup != null&&canShoot&&!justShot) {
+            print("You tried to throw it");
+            pickup.GetComponent<Rigidbody>().isKinematic = false;
             pickup.GetComponent<Rigidbody>().useGravity = true;
+            //pickup.GetComponent<Rigidbody>().velocity = Vector3.forward * 10 + Vector3.up * 10;
+            pickup.GetComponent<Rigidbody>().angularVelocity = Vector3.forward * 100000;
+            pickup.GetComponent<Rigidbody>().AddForce(Vector3.up* 10 + Vector3.left*10, ForceMode.Impulse);
+            canShoot = false;
+            justShot = true;
+            
+        }
+        if (!canShoot && justShot)
+        {
+            if (pickup.transform.position.y < .5)
+            {
+                print("I'm dying");
+                pickup.GetComponent<Rigidbody>().useGravity = false;
+                pickup.GetComponent<Rigidbody>().isKinematic = true;
+                pickup.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                pickup.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            }
         }
 	}
     private void OnTriggerEnter(Collider collider)
     {
         pickup = collider.gameObject;
+        print("You interacted with it");
     }
     private void OnTriggerExit(Collider collider)
     {
         pickup = null;
     }
- 
 }
--- Throwing Mechanics Test/Assets/throw.js
+++ Throwing Mechanics Test/Assets/throw.js
@@ -1,53 +1,33 @@
 ﻿#pragma strict
-
-//var mc : GameObject; // The main camera
-var g: boolean = false;
-GetComponent. < Rigidbody > ().useGravity = false; // Gravity
-var canShoot: boolean = false;
-var justShot: boolean = false;
-var cc: CharacterController; // The player
+// import UnityEngine.VR;
+// //var mc : GameObject; // The main camera
+// var g: boolean = false;
+// GetComponent. < Rigidbody > ().useGravity = false; // Gravity
+// var canShoot: boolean = false;
+// var justShot: boolean = false;
+// var cc: VRDevice; // The player
 
 function Start() {}
 
 function Update() {
     {
-        var distance: float;
-        distance = Vector3.Distance(cc.transform.position, GetComponent. < Rigidbody > ().transform.position);
-
-        if (Input.GetKey("p") && !canShoot && !justShot) {
-            canShoot = true;
-            transform.position = cc.transform.position + Vector3.up + Vector3.back;
-            // If the player is close enough by the sphere and he didn't just shot, the sphere will be picked up.
-            // If the justShot boolean wouldn't be there, you would pick up the sphere immediataly after shooting and you
-            // could not throw it.
-        }
-
-        // if (g) {
-        //     GetComponent. < Rigidbody > ().AddForce(Physics.gravity * GetComponent. < Rigidbody > ().mass); // gravity
-        // }
-
-        // if (canShoot &&  !justshot) {
-        //     transform.position = cc.transform.position + cc.transform.forward * 2; // teleports the sphere in front of the player
-        //     GetComponent. < Rigidbody > ().velocity = Vector3.one; // Sets the velocity
-        //     GetComponent. < Rigidbody > ().angularVelocity = Vector3.one*10; // and the rotation of the sphere to zero (rotation is not necessary if you work with a shere).
-        // }
-
-        if (Input.GetKey("b") && canShoot) {
-            print("I made It");
-            g = true;
-            GetComponent. < Rigidbody > ().AddForce(cc.transform.up * 10 + cc.transform.forward * 10, ForceMode.Impulse);
-            GetComponent. < Rigidbody > ().useGravity = true;
-            GetComponent. < Rigidbody > ().angularVelocity = Vector3.one * 5; // shoot
-            canShoot = false;
-            justShot = true;
-        }
-
-        if (transform.position.y < .1 && justShot) {
-            justShot = false;
-            GetComponent. < Rigidbody > ().angularVelocity = Vector3.zero;
-            GetComponent. < Rigidbody > ().velocity = Vector3.zero;
-            GetComponent. < Rigidbody > ().useGravity = false; // reset the justShot boolean, if the sphere is far enough
-        }
+        //     InputTracking input =
+        //     transform.position = cc.GetLocalPosition() + Vector3.up + Vector3.back;
+        //     // If the player is close enough by the sphere and he didn't just shot, the sphere will be picked up.
+        //     // If the justShot boolean wouldn't be there, you would pick up the sphere immediataly after shooting and you
+        //     // could not throw it.
+        //
+        //
+        // // if (g) {
+        // //     GetComponent. < Rigidbody > ().AddForce(Physics.gravity * GetComponent. < Rigidbody > ().mass); // gravity
+        // // }
+        //
+        // // if (canShoot &&  !justshot) {
+        // //     transform.position = cc.transform.position + cc.transform.forward * 2; // teleports the sphere in front of the player
+        // //     GetComponent. < Rigidbody > ().velocity = Vector3.one; // Sets the velocity
+        // //     GetComponent. < Rigidbody > ().angularVelocity = Vector3.one*10; // and the rotation of the sphere to zero (rotation is not necessary if you work with a shere).
+        // // }
+        //
 
     }
 }
