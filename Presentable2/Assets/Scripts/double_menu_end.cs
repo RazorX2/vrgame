84bd8ff7692a3ca4e1520bc8aa54806b8b70950f
Vishal Tandale <2017vtandale@tjhsst.edu>
2016-08-04 14:21:51 -0400

    Pre pull

---
+++ Presentable2/Assets/Scripts/double_menu_end.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class double_menu_end : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Awake()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		SteamVR_Controller.Device left = SteamVR_Controller.Input(3);
+		SteamVR_Controller.Device right = SteamVR_Controller.Input(4);
+
+		if (left.GetPress(SteamVR_Controller.ButtonMask.ApplicationMenu) && right.GetPress(SteamVR_Controller.ButtonMask.ApplicationMenu))
+		{
+			Debug.Log("both menu down");
+			SceneManager.LoadScene(0);
+		}
+	}
+}
