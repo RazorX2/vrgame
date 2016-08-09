b10803c4d50fa3d391548fe9bd91e9de06a3a200
A. Shi <shi.andrewc@gmail.com>
2016-08-09 09:36:05 -0400

    idk what changed...

--- test proj/Assets/Scripts/virus_spawn.cs
+++ test proj/Assets/Scripts/virus_spawn.cs
@@ -3,22 +3,30 @@ using System.Collections;
 
 public class virus_spawn : MonoBehaviour {
 	public float delay; //time between spawns
-	public GameObject virus; //complete virus: needs a single copy to start with.
+	public GameObject enemy1, enemy2; //complete virus: needs a single copy to start with.
+    private int speed;
+    private Random rnd;
+    private GameObject[] enemies;
 
-	void Start () {
+    void Start () {
 		StartCoroutine(activatePoints());
+        enemies = new GameObject[] { enemy1, enemy2 };
+        rnd = new Random();
+
 	}
 
 	IEnumerator activatePoints(){
+        Debug.Log("Type of child array"+transform.GetType());
 		foreach (Transform child in transform) {
-			StartCoroutine (spawnViruses (delay, child.position, child.rotation));
+            //Debug.Log("random Enemy:"+rnd.Range((float)0,(float)2.0));
+            StartCoroutine (spawnViruses(delay, child.position, child.rotation, enemy2  )    );
 			yield return new WaitForSeconds (delay/transform.childCount);
 		}
 	}
 
-	IEnumerator spawnViruses(float delay, Vector3 position, Quaternion rotation){
+	IEnumerator spawnViruses(float delay, Vector3 position, Quaternion rotation,GameObject enemy){
 		while (true) {
-			GameObject nextvirus = Instantiate<GameObject> (virus);
+			GameObject nextvirus = Instantiate<GameObject> (enemy);
 			nextvirus.transform.position = Vector3.Scale(position,(Vector3.right+Vector3.forward));
 			nextvirus.transform.rotation = rotation;
 			nextvirus.AddComponent<Rigidbody> ();
@@ -29,6 +37,10 @@ public class virus_spawn : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+    public void TurnOff()
+    {
+        this.enabled = false;
+    }
 }
